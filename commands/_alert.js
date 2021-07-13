914ab57806f1f66375a76991067688e64605930f
Radin <78489643+mohdradin@users.noreply.github.com>
2021-07-13 18:07:14 +0530

    Create _alert.js

---
+++ commands/_alert.js
@@ -0,0 +1,25 @@
+/*CMD
+  command: /alert
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+// Automatic fix
+var is_alert;
+
+// Automatic fix
+var is_alert;
+
+is_alert = ( params!="top")
+
+// help - https://core.telegram.org/bots/api#answercallbackquery
+Api.answerCallbackQuery({
+  callback_query_id: request.id,
+  text: "Hello, user! This is my alert!",
+  show_alert: is_alert 
+})
